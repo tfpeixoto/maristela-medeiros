4731b208638bcad10064640a21002390e02fac63
Thiago Peixoto <tfpeixoto@gmail.com>
2022-10-17 22:41:33 -0300

    Performance enhancements

--- wp-content/themes/maristela-medeiros/front-page.php
+++ wp-content/themes/maristela-medeiros/front-page.php
@@ -7,197 +7,210 @@ get_header(); ?>
 <!--MODAL-->
 <?php require_once("modal.php"); ?>
 
-	<!--BANNER-->
-	<div class="jumbotron">
-		<div class="container">
-		<div class="row">
-			<div class="col-12 col-md-8 text-center text-md-left">
-				<?php query_posts('page_id=5'); if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<!--BANNER-->
+<div class="jumbotron">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-md-8 text-center text-md-left">
+        <?php query_posts('page_id=5');
+        if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-					<h1 class="font-weight-bold"><?php the_content(); ?></h1>
+            <h1 class="font-weight-bold"><?php the_content(); ?></h1>
 
-				<?php endwhile; endif; ?>
-				<?php wp_reset_query(); ?>
+        <?php endwhile;
+        endif; ?>
+        <?php wp_reset_query(); ?>
 
-				<a role="button" href="#servicos" class="btn btn-lg btn-outline-light mt-2">Conheça os serviços</a>
-			</div>
-		</div>
-	</div>
+        <a role="button" href="#servicos" class="btn btn-lg btn-outline-light mt-2">Conheça os serviços</a>
+      </div>
+    </div>
+  </div>
+</div>
 </header>
 
 <main>
-	<!--QUEM SOU-->
-	<div id="quem-sou" class="py-5">
-		<div class="container">
-			<div class="row d-flex align-items-center justify-content-center">
-				<?php query_posts('page_id=2'); if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+  <!--QUEM SOU-->
+  <div id="quem-sou" class="py-5">
+    <div class="container">
+      <div class="row d-flex align-items-center justify-content-center">
+        <?php query_posts('page_id=2');
+        if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-					<div class="col-10 col-md-5">
-						<img src="<?php the_post_thumbnail_url(); ?>" class="card-img-top" alt="Nutricionista Maristela Medeiros, especialista em segurança dos alimentos" />
-					</div>
+            <div class="col-10 col-md-5">
+              <img src="<?php the_post_thumbnail_url(); ?>" class="card-img-top" width="445" height="474" alt="Nutricionista Maristela Medeiros, especialista em segurança dos alimentos" />
+            </div>
 
-					<div class="col-12 col-md-7 text-center text-md-left">
-						<h2 class="text-primary pb-4 mt-4 mt-md-0"><?php the_title(); ?></h2>
+            <div class="col-12 col-md-7 text-center text-md-left">
+              <h2 class="text-primary pb-4 mt-4 mt-md-0"><?php the_title(); ?></h2>
 
-						<?php the_content(); ?>
+              <?php the_content(); ?>
 
-				<?php endwhile; endif; ?>
-				<?php wp_reset_query(); ?>
-			</div>		
-		</div>
-	</div>
+          <?php endwhile;
+        endif; ?>
+          <?php wp_reset_query(); ?>
+            </div>
+      </div>
+    </div>
 
-	<!--SERVIÇOS-->
-	<div id="servicos" class="bg-palha py-5">
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
-					<h2 class="text-primary text-center">Serviços</h2>
+    <!--SERVIÇOS-->
+    <div id="servicos" class="bg-palha py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h2 class="text-primary text-center">Serviços</h2>
 
-					<div class="card-deck">
-						<div class="card border-0 rounded text-center shadow-sm">
-							<img class="card-img-top" src="<?php bloginfo("template_url"); ?>/images/icone-auditorias.png" alt="Imagem de capa do card">
+            <div class="card-deck">
+              <div class="card border-0 rounded text-center shadow-sm">
+                <img class="card-img-top" src="<?php bloginfo("template_url"); ?>/images/icone-auditorias.png" alt="Imagem de capa do card">
 
-							<div class="card-body">
-								<h3 class="card-title text-marrom">Auditorias</h3>
-								<p class="card-text">Apuração de possíveis inconformidades e situações irregulares pelos padrões determinados pela Vigilância Sanitária, visando a prevenção de autuações e riscos de contaminação.</p>
-								<a href="auditorias" type="button" class="btn btn-primary btn-lg shadow-sm mb-3">Saiba mais</a>
-							</div>
-						</div>
+                <div class="card-body">
+                  <h3 class="card-title text-marrom">Auditorias</h3>
+                  <p class="card-text">Apuração de possíveis inconformidades e situações irregulares pelos padrões determinados pela Vigilância Sanitária, visando a prevenção de autuações e riscos de contaminação.</p>
+                  <a href="auditorias" type="button" class="btn btn-primary btn-lg shadow-sm mb-3">Saiba mais</a>
+                </div>
+              </div>
 
-						<div class="card border-0 rounded text-center shadow-sm">
-							<img class="card-img-top" src="<?php bloginfo("template_url"); ?>/images/icone-consultorias.png" alt="Imagem de capa do card">
+              <div class="card border-0 rounded text-center shadow-sm">
+                <img class="card-img-top" src="<?php bloginfo("template_url"); ?>/images/icone-consultorias.png" alt="Imagem de capa do card">
 
-							<div class="card-body">
-								<h3 class="card-title text-marrom">Consultorias</h3>
-								<p class="card-text">Orientação e acompanhamento para aperfeiçoamentos nas técnicas, segurança dos alimentos, planejamento, precificação, assegurando a aplicação das melhores práticas de produção.</p>
-								<a href="consultorias" type="button" class="btn btn-primary btn-lg shadow-sm mb-3">Saiba mais</a>
-							</div>
-						</div>
+                <div class="card-body">
+                  <h3 class="card-title text-marrom">Consultorias</h3>
+                  <p class="card-text">Orientação e acompanhamento para aperfeiçoamentos nas técnicas, segurança dos alimentos, planejamento, precificação, assegurando a aplicação das melhores práticas de produção.</p>
+                  <a href="consultorias" type="button" class="btn btn-primary btn-lg shadow-sm mb-3">Saiba mais</a>
+                </div>
+              </div>
 
-						<div class="card border-0 rounded text-center shadow-sm">
-							<img class="card-img-top" src="<?php bloginfo("template_url"); ?>/images/icone-treinamentos.png" alt="Imagem de capa do card">
+              <div class="card border-0 rounded text-center shadow-sm">
+                <img class="card-img-top" src="<?php bloginfo("template_url"); ?>/images/icone-treinamentos.png" alt="Imagem de capa do card">
 
-							<div class="card-body">
-								<h3 class="card-title text-marrom">Treinamentos</h3>
-								<p class="card-text">Capacitações técnicas para empresas, com conteúdo adaptável de acordo com a necessidade do cliente e porte da equipe, apoiando o desenvolvimento e padronização de processos.</p>
-								<a href="servicos" type="button" class="btn btn-primary btn-lg shadow-sm mb-3">Saiba mais</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+                <div class="card-body">
+                  <h3 class="card-title text-marrom">Treinamentos</h3>
+                  <p class="card-text">Capacitações técnicas para empresas, com conteúdo adaptável de acordo com a necessidade do cliente e porte da equipe, apoiando o desenvolvimento e padronização de processos.</p>
+                  <a href="servicos" type="button" class="btn btn-primary btn-lg shadow-sm mb-3">Saiba mais</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
-	<!--CTA-->
-	<div id="cta" class="bg-cinza text-white py-5 py-md-3">
-		<div class="container">
-			<div class="row">
-				<?php query_posts('page_id=26'); if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <!--CTA-->
+    <div id="cta" class="bg-cinza text-white py-5 py-md-3">
+      <div class="container">
+        <div class="row">
+          <?php query_posts('page_id=26');
+          if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-					<div class="col-12 col-md-5">
-						<img src="<?php the_post_thumbnail_url(); ?>" class="img-fluid" alt="Atendimento nutricional especializado para empresas" />
-					</div>
+              <div class="col-12 col-md-5">
+                <img src="<?php the_post_thumbnail_url(); ?>" width="445" height="340" class="img-fluid" alt="Atendimento nutricional especializado para empresas" />
+              </div>
 
-					<div class="col-12 col-md-7 align-self-center text-center text-md-left">
-						<h3><?php the_content(); ?></h3>
+              <div class="col-12 col-md-7 align-self-center text-center text-md-left">
+                <h3><?php the_content(); ?></h3>
 
-						<a href="#" class="btn btn-primary btn-lg mt-3" role="button" data-toggle="modal" data-target="#SolicitaContato">Entre em contato</a>
-					</div>
+                <a href="#" class="btn btn-primary btn-lg mt-3" role="button" data-toggle="modal" data-target="#SolicitaContato">Entre em contato</a>
+              </div>
 
-				<?php endwhile; endif; ?>
-				<?php wp_reset_query(); ?>
-			</div>
-		</div>
-	</div>
+          <?php endwhile;
+          endif; ?>
+          <?php wp_reset_query(); ?>
+        </div>
+      </div>
+    </div>
 
-	<!--DEPOIMENTOS-->
-	<div id="depoimentos" class="py-5">
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
-					<h2 class="text-primary text-center">Depoimentos</h2>
-					<p class="text-center font-italic mb-5">O que dizem os clientes</p>
+    <!--DEPOIMENTOS-->
+    <div id="depoimentos" class="py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h2 class="text-primary text-center">Depoimentos</h2>
+            <p class="text-center font-italic mb-5">O que dizem os clientes</p>
 
-					<div class="row">
-						<?php query_posts('post_type=depoimentos&post_per_page=2'); ?>						
-						<?php if(have_posts()): ?>
-						<?php while(have_posts()): the_post(); ?>
+            <div class="row">
+              <?php query_posts('post_type=depoimentos&post_per_page=2'); ?>
+              <?php if (have_posts()) : ?>
+                <?php while (have_posts()) : the_post(); ?>
 
-							<div class="col-12 col-md-6 mb-5 mb-md-0">
-								<div class="media row d-flex justify-content-center justify-content-md-left align-items-center">
-									<a href="#" data-toggle="modal" data-target="#tab-<?php the_id(); ?>"><?php the_post_thumbnail( 'img-depoimento' ); ?></a>
-									<div class="media-body col-12 col-md-8 text-center text-md-left">							    
-										<a href="#" data-toggle="modal" data-target="#tab-<?php the_id(); ?>"><?php the_content(); ?></a>
-										<h5 class="mt-3 font-weight-bold"><a href="#" data-toggle="modal" data-target="#tab-<?php the_id(); ?>"><?php the_title(); ?></a></h5>
-									</div>
-								</div>						
-							</div>
+                  <div class="col-12 col-md-6 mb-5 mb-md-0">
+                    <div class="media row d-flex justify-content-center justify-content-md-left align-items-center">
+                      <a href="#" data-toggle="modal" data-target="#tab-<?php the_id(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('img-depoimento'); ?></a>
 
-							<div class="modal" id="tab-<?php the_id(); ?>" tabindex="-1" role="dialog">
-								<div class="modal-dialog" role="document">
-									<div class="modal-content">
-										<div class="modal-header">
-											<h5 class="modal-title"><?php the_title(); ?></h5>
-											<button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-												<span aria-hidden="true">&times;</span>
-											</button>
-										</div>
-										<div class="modal-body">
-											<?php the_field('video'); ?>
+                      <div class="media-body col-12 col-md-8 text-center text-md-left">
+                        <a href="#" data-toggle="modal" data-target="#tab-<?php the_id(); ?>"><?php the_content(); ?></a>
 
-											<p class="text-center">Pause a reprodução do vídeo antes de fechar a janela</p>
-										</div>
-										<div class="modal-footer">
-											<button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-										</div>
-									</div>
-								</div>
-							</div>
+                        <h3 class="h5 mt-3 font-weight-bold">
+                          <a href="#" data-toggle="modal" data-target="#tab-<?php the_id(); ?>"><?php the_title(); ?></a>
+                        </h3>
+                      </div>
+                    </div>
+                  </div>
 
-							<?php endwhile; endif; ?>
-							<?php wp_reset_query(); ?>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+                  <div class="modal" id="tab-<?php the_id(); ?>" tabindex="-1" role="dialog">
+                    <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h3 class="modal-title h5"><?php the_title(); ?></h3>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+                          <?php the_field('video'); ?>
 
-	<!--BLOG-->
-	<div id="blog" class="bg-primary mt-4 pb-5">
-		<div class="container">
-			<div class="row d-flex justify-content-center">
-				<div class="col-12 col-md-4 text-center text-md-left">
-					<h2 class="text-primary mb-5">Blog</h2>
-					<p class="mb-5">Aqui você fica informado sobre todas as novidades do mundo da nutrição corporativa.</p>
+                          <p class="text-center">Pause a reprodução do vídeo antes de fechar a janela</p>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
 
-					<a role="button" href="/blog" class="btn btn-outline-light btn-lg mt-md-5 mb-md-0 my-4 text-uppercase">Ver todos os posts</a>
-				</div>
-				
-				<div class="col-10 col-md-8">
-					<div class="card-deck">
-						 <?php $query = new WP_Query( 'posts_per_page=2' ); ?>
-						 <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+              <?php endwhile;
+              endif; ?>
+              <?php wp_reset_query(); ?>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
-							<div class="card border-0 rounded shadow-sm">
-								<?php the_post_thumbnail( 'img-post' ); ?>
+  <!--BLOG-->
+  <div id="blog" class="bg-primary mt-4 pb-5">
+    <div class="container">
+      <div class="row d-flex justify-content-center">
+        <div class="col-12 col-md-4 text-center text-md-left">
+          <h2 class="text-primary mb-5">Blog</h2>
+          <p class="mb-5">Aqui você fica informado sobre todas as novidades do mundo da nutrição corporativa.</p>
 
-								<div class="card-body">
-									<h3 class="card-title"><a href="<?php the_permalink(); ?>" class="text-marrom-claro"><?php the_title(); ?></a></h3>
+          <a role="button" href="/blog" class="btn btn-outline-light btn-lg mt-md-5 mb-md-0 my-4 text-uppercase">Ver todos os posts</a>
+        </div>
 
-									<p class="card-text"><?php the_excerpt(); ?></p>
+        <div class="col-10 col-md-8">
+          <div class="card-deck">
+            <?php $query = new WP_Query('posts_per_page=2'); ?>
+            <?php if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
 
-									<a href="<?php the_permalink(); ?>" class="text-uppercase font-weight-bold leia-mais">Ler mais ></a>
-								</div>
-							</div>
+                <div class="card border-0 rounded shadow-sm">
+                  <?php the_post_thumbnail('img-post'); ?>
 
-						<?php endwhile; endif; ?>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+                  <div class="card-body">
+                    <h3 class="card-title"><a href="<?php the_permalink(); ?>" class="text-marrom-claro"><?php the_title(); ?></a></h3>
 
-<?php get_footer(); ?>
+                    <p class="card-text"><?php the_excerpt(); ?></p>
+
+                    <a href="<?php the_permalink(); ?>" class="text-uppercase font-weight-bold leia-mais">Ler mais ></a>
+                  </div>
+                </div>
+
+            <?php endwhile;
+            endif; ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php get_footer(); ?>
--- wp-content/themes/maristela-medeiros/functions.php
+++ wp-content/themes/maristela-medeiros/functions.php
@@ -1,73 +1,129 @@
 <?php
 
 //Chamar a tag title
-function maristela_title_tag() {
-	add_theme_support('title-tag');
+function maristela_title_tag()
+{
+  add_theme_support('title-tag');
 }
 add_action('after_setup_theme', 'maristela_title_tag');
 
 // Prevenir o erro na tag title em versões antigas
 if (!function_exists('wp_render_title_tag')) {
-	function maristela_render_title() {
-		?>
-		<title><?php wp_title('|', true, 'right'); ?></title>
-		<?php
-	}
-	add_action('wp_head', 'maristela_render_title');
+  function maristela_render_title()
+  {
+?>
+    <title><?php wp_title('|', true, 'right'); ?></title>
+<?php
+  }
+  add_action('wp_head', 'maristela_render_title');
 }
 
 // Registrar o Custom Navigation Walker
 require_once get_template_directory() . '/wp_bootstrap_navwalker.php';
 
 // Tamanho do resumo
-function custom_excerpt_length( $length ) {
-	return 20;
+function custom_excerpt_length($length)
+{
+  return 20;
 }
-add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+add_filter('excerpt_length', 'custom_excerpt_length', 999);
 
 // Registrar o Menu
-register_nav_menus( array(
-	'principal' => __('Menu Principal', 'maristela'),
-	'social' => __('Menu Social', 'maristela'),
+register_nav_menus(array(
+  'principal' => __('Menu Principal', 'maristela'),
+  'social' => __('Menu Social', 'maristela'),
 ));
 
 // Registrar os Post Types
-function meus_posts_type(){
-	// Depoimentos
-	register_post_type('depoimentos',
-		array(
-			'labels' 					=> array(
-				'name' 					=> __('Depoimentos'),
-				'singular_name' => __('Depoimento'),
-			),
-			'public' 					=> true,
-			'has_archive' 		=> false,
-			'menu_icon'				=> 'dashicons-format-quote',
-			'supports'				=> array('title', 'editor', 'thumbnail', 'page-attributes'),
-		)
-	);
+function meus_posts_type()
+{
+  // Depoimentos
+  register_post_type(
+    'depoimentos',
+    array(
+      'labels'           => array(
+        'name'           => __('Depoimentos'),
+        'singular_name' => __('Depoimento'),
+      ),
+      'public'           => true,
+      'has_archive'     => false,
+      'menu_icon'        => 'dashicons-format-quote',
+      'supports'        => array('title', 'editor', 'thumbnail', 'page-attributes'),
+    )
+  );
 
-	// Serviços
-	register_post_type('servicos',
-		array(
-			'labels' 					=> array(
-				'name' 					=> __('Serviços'),
-				'singular_name' => __('Serviço'),
-			),
-			'public' 					=> true,
-			'has_archive' 		=> false,
-			'menu_icon'				=> 'dashicons-admin-appearance',
-			'supports'				=> array('title', 'editor', 'thumbnail', 'page-attributes'),
-			'taxonomies'      => array( 'category' ),
-		)
-	);
+  // Serviços
+  register_post_type(
+    'servicos',
+    array(
+      'labels'           => array(
+        'name'           => __('Serviços'),
+        'singular_name' => __('Serviço'),
+      ),
+      'public'           => true,
+      'has_archive'     => false,
+      'menu_icon'        => 'dashicons-admin-appearance',
+      'supports'        => array('title', 'editor', 'thumbnail', 'page-attributes'),
+      'taxonomies'      => array('category'),
+    )
+  );
 }
 add_action('init', 'meus_posts_type');
 
 // Ativer thumbnail
 add_theme_support('post-thumbnails');
-add_image_size( 'img-post', 370, 200 ); // 300 pixels wide (and unlimited height)
-add_image_size( 'img-post-wide', 700 ); 
-add_image_size( 'img-depoimento', 150, 150 ); 
+add_image_size('img-post', 370, 200); // 300 pixels wide (and unlimited height)
+add_image_size('img-post-wide', 700);
+add_image_size('img-depoimento', 150, 150);
 
-?>
+// Remover o block-library/style.css
+function wpassist_remove_block_library_css()
+{
+  wp_dequeue_style('wp-block-library');
+}
+add_action('wp_enqueue_scripts', 'wpassist_remove_block_library_css');
+
+/**
+ * Disable the emoji's
+ */
+function disable_emojis()
+{
+  remove_action('wp_head', 'print_emoji_detection_script', 7);
+  remove_action('admin_print_scripts', 'print_emoji_detection_script');
+  remove_action('wp_print_styles', 'print_emoji_styles');
+  remove_action('admin_print_styles', 'print_emoji_styles');
+  remove_filter('the_content_feed', 'wp_staticize_emoji');
+  remove_filter('comment_text_rss', 'wp_staticize_emoji');
+  remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+
+  // Remove from TinyMCE
+  add_filter('tiny_mce_plugins', 'disable_emojis_tinymce');
+}
+add_action('init', 'disable_emojis');
+
+/**
+ * Filter out the tinymce emoji plugin.
+ */
+function disable_emojis_tinymce($plugins)
+{
+  if (is_array($plugins)) {
+    return array_diff($plugins, array('wpemoji'));
+  } else {
+    return array();
+  }
+}
+
+// send headers seo
+function hot_set_headers_seo()
+{
+  header('Strict-Transport-Security: max-age=31536000; includeSubDomains; preload');
+  header('X-Frame-Options: sameorigin');
+  header('X-XSS-Protection: 1; mode=block');
+  header('X-Content-Type-Options: nosniff');
+  header('Referrer-Policy: same-origin');
+  header("Cache-Control: no-cache, no-store, must-revalidate");
+  header("Pragma: no-cache");
+  header("Expires: 0");
+  // header('Content-Security-Policy': 'default-src self');
+}
+add_action('send_headers', 'hot_set_headers_seo');
