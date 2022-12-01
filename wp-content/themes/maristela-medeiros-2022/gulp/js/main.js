8c29d3a538725e684684877c072f477c0e7df6a9
Thiago Peixoto <tfpeixoto@gmail.com>
2022-12-01 00:03:01 -0300

    feat: passive events and css order

--- wp-content/themes/maristela-medeiros-2022/gulp/js/main.js
+++ wp-content/themes/maristela-medeiros-2022/gulp/js/main.js
@@ -33,16 +33,34 @@ btnContatoModal.addEventListener('click', function () {
 })
 
 // passive events
-const events = ['touchstart', 'touchmove', 'wheel', 'mousewheel']
-function passiveEvent(events) {
-  events.forEach(mineEvent => {
-    jQuery.event.special.mineEvent = {
-      setup: function (_, ns, handle) {
-        this.addEventListener(mineEvent, handle, {
-          passive: true
-        });
-      }
-    }
-  })
-}
-passiveEvent(events)
+jQuery.event.special.touchstart = {
+  setup: function (_, ns, handle) {
+    this.addEventListener("touchstart", handle, {
+      passive: !ns.includes("noPreventDefault")
+    });
+  }
+};
+
+jQuery.event.special.touchmove = {
+  setup: function (_, ns, handle) {
+    this.addEventListener("touchmove", handle, {
+      passive: !ns.includes("noPreventDefault")
+    });
+  }
+};
+
+jQuery.event.special.wheel = {
+  setup: function (_, ns, handle) {
+    this.addEventListener("wheel", handle, {
+      passive: true
+    });
+  }
+};
+
+jQuery.event.special.mousewheel = {
+  setup: function (_, ns, handle) {
+    this.addEventListener("mousewheel", handle, {
+      passive: true
+    });
+  }
+};
